b6a687710f44963c99c51a4944ed322c1fb6f315
hdorothea <dorothea.hormann@gmail.com>
2017-08-09 20:06:28 -0400

    Add plane identity message over map

---
+++ src/components/Plane.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { keysToIndexApp } from '../utils/ApiUtils';
+import countryToCode from '../data/countryToCode';
+
+export default class Plane extends React.Component {
+  constructor() {
+    super();
+    this.state = { mouseOver: false };
+    this.toggleMouseOver = this.toggleMouseOver.bind(this);
+  }
+
+  toggleMouseOver() {
+    this.setState(previousState => ({
+      mouseOver: !previousState.mouseOver,
+    }));
+  }
+
+  // visibility={this.state.mouseOver ? 'visible' : 'hidden'}
+
+  render() {
+    return (
+      <g transform={this.props.transform}>
+        <g visibility={this.state.mouseOver ? 'visible' : 'hidden'}>
+          <text style={{ fill: 'black' }}>
+            {this.props.observation[keysToIndexApp.airlineCountry]}
+          </text>
+          <text dy="-1em" style={{ fill: 'black' }}>
+            {this.props.observation[keysToIndexApp.id]}
+          </text>
+        </g>
+        <image
+          xlinkHref={`countries/${countryToCode[
+            this.props.observation[keysToIndexApp.airlineCountry]
+          ]
+            ? countryToCode[this.props.observation[keysToIndexApp.airlineCountry]]
+            : this.props.observation[keysToIndexApp.airlineCountry]}.png`}
+          width="15"
+          height="15"
+          onMouseOver={this.toggleMouseOver}
+          onMouseOut={this.toggleMouseOver}
+        />
+      </g>
+    );
+  }
+}
--- src/components/WorldMap.js
+++ src/components/WorldMap.js
@@ -1,26 +1,19 @@
 import React from 'react';
 import { geoMercator, geoAzimuthalEqualArea, geoPath } from 'd3-geo';
 import worlddata from '../data/world';
+import Plane from './Plane';
 import { keysToIndexApp } from '../utils/ApiUtils';
-import countryToCode from '../data/countryToCode';
 
 export default function WorldMap({ observations }) {
   // longtitude to rotate
   // latitude with center
-  // to have this responsive scale the projection find out correct width and height for desktop
-  // technically to have this zoom in scale the projection and set x any y of the aspect ratio
-  const projection = geoMercator().scale(150).rotate([80, 0]).center([0, 65]);
+  // to have this responsive: scale the projection find out correct width and height for desktop
+  // technically to have this zoom in: scale the projection and set x any y of the aspect ratio
+  const projection = geoMercator().rotate([80, 0]).center([0, 65]);
   const pathGenerator = geoPath().projection(projection);
-  observations.map((observation) => {
-    if (!countryToCode[observation[keysToIndexApp.airlineCountry]]) {
-      console.log(observation[keysToIndexApp.airlineCountry]);
-    }
-  });
+
   return (
-    <svg
-      viewBox="0 0 1000 700"
-      preserveAspectRatio="xMinYMin"
-    >
+    <svg viewBox="0 0 1000 700" preserveAspectRatio="xMinYMin">
       {worlddata.features.map((d, i) =>
         (<path
           key={`path-${i}`}
@@ -29,10 +22,9 @@ export default function WorldMap({ observations }) {
         />),
       )}
       {observations.map(observation =>
-        (<image
-          xlinkHref={`countries/${countryToCode[observation[keysToIndexApp.airlineCountry]]}.png`}
-          width="15"
-          height="15"
+        (<Plane
+          key={observation[keysToIndexApp.id]}
+          observation={observation}
           transform={`translate(${projection([
             observation[keysToIndexApp.longtitude],
             observation[keysToIndexApp.latitude],
