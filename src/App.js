147a034f01abb8bae511e679f6db0baf3b08d064
hdorothea <dorothea.hormann@gmail.com>
2017-08-15 11:28:03 -0400

    Extract header component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,17 @@
 import React from 'react';
-import FontAwesome from 'react-fontawesome';
 
 import './App.css';
-import '../node_modules/font-awesome/css/font-awesome.min.css';
 import DashboardContainer from './containers/DashboardContainer';
-import FilterContainer from './containers/FilterContainer';
-import ShowModalContainer from './containers/ShowModalContainer';
-import RangeFilterContainer from './containers/RangeFilterContainer';
+import FilterDashContainer from './containers/FilterDashContainer';
+import Header from './components/Header';
+
 
 function App() {
   return (
-    <div id="app-container">
-      <div id="app-header">
-        <div className="title">
-          {' '}<span>Planes</span>InTheSky{' '}
-        </div>
-        <div className="link-bar">
-          <a href="https://github.com/hdorothea/PlanesInTheSky">
-            <FontAwesome name="fa-github" className="fa fa-github" />
-            Source Code on Github
-          </a>
-          <a href="https://opensky-network.org/apidoc/rest.html">
-            <FontAwesome name="fa-plane" className="fa fa-plane" />
-            Open Sky Network Api
-          </a>
-        </div>
-      </div>
-      <div className="app-content">
-        <div className="app-all-filters">
-          <RangeFilterContainer />
-          <FilterContainer />
-          <ShowModalContainer />
-        </div>
+    <div className="app">
+      <Header />
+      <div>
+        <FilterDashContainer />
         <DashboardContainer />
       </div>
     </div>
---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import FontAwesome from 'react-fontawesome';
+
+import './Header.css';
+
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="title-container">
+        <div className="title">
+          {' '}<span> Planes </span> InTheSky{' '}
+        </div>
+      </div>
+      <div className="link-bar">
+        <a href="https://github.com/hdorothea/PlanesInTheSky">
+          <FontAwesome name="fa-github" className="fa fa-github" />
+          Source Code on Github
+        </a>
+        <a href="https://opensky-network.org/apidoc/rest.html">
+          <FontAwesome name="fa-plane" className="fa fa-plane" />
+          Open Sky Network Api
+        </a>
+      </div>
+    </div>
+  );
+}
