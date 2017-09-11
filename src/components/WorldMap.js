143587d5051df97975697f3ac4b450bec72c76f8
hdorothea <dorothea.hormann@gmail.com>
2017-09-11 19:39:06 -0400

    Make WorldMap code a little nicer

--- src/components/WorldMap.js
+++ src/components/WorldMap.js
@@ -12,73 +12,59 @@ export default class WorldMap extends React.Component {
 
   render() {
     const { observations, latitudeRangeFilter, longtitudeRangeFilter, showAll } = this.props;
-    // zooming see: https://stackoverflow.com/a/14691788/7174408
     const width = 800;
     const height = 700;
 
-    const projection = geoMercator().scale(1).translate([0, 0]);
+    const projection = geoMercator()
+      .scale(120)
+      .translate([400, 350]);
     const pathGenerator = geoPath().projection(projection);
 
-    const b = [
-      projection([
-        Math.max(longtitudeRangeFilter.min, -179.9),
-        Math.max(latitudeRangeFilter.min, -89.9),
-      ]),
-      projection([
-        Math.min(longtitudeRangeFilter.max, 179.9),
-        Math.min(latitudeRangeFilter.max, 89.9),
-      ]),
-    ];
-    const s = 0.95 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][0]) / height);
-    const t = [(width - s * (b[1][0] + b[0][0])) / 2, (height - s * (b[1][1] + b[0][1])) / 2];
-    projection.scale(s).translate(t);
+    let {
+      currentMin: longtitudeCurrentMin,
+      currentMax: longtitudeCurrentMax,
+    } = longtitudeRangeFilter;
+    let { currentMin: latitudeCurrentMin, currentMax: latitudeCurrentMax } = latitudeRangeFilter;
+
+    longtitudeCurrentMin = Math.max(longtitudeCurrentMin, -179.9);
+    longtitudeCurrentMax = Math.min(longtitudeCurrentMax, 179.9);
+    latitudeCurrentMin = Math.max(latitudeCurrentMin, -89.9);
+    latitudeCurrentMax = Math.min(latitudeCurrentMax, 89.9);
+
     return (
       <div id="world-map-container">
         <svg viewBox={`0, -120 ${width} ${height}`} preserveAspectRatio="xMinYMin">
-          {worlddata.features.map((d, i) =>
-            (<path
+          {worlddata.features.map((d, i) => (
+            <path
               key={`path-${i}`}
               d={pathGenerator(d)}
               style={{ fill: 'palegoldenrod', stroke: 'silver' }}
-            />),
-          )}
-          {observations.map(observation =>
-            (<Plane
+            />
+          ))}
+          {observations.map(observation => (
+            <Plane
               key={observation[keysToIndexApp.id]}
               observation={observation}
               transform={`translate(${projection([
                 observation[keysToIndexApp.longtitude],
                 observation[keysToIndexApp.latitude],
               ])})`}
-            />),
-          )}
-          {!showAll
-            ? <polyline
+            />
+          ))}
+          {!showAll ? (
+            <polyline
               fill="none"
               stroke="black"
               points={`
-            ${projection([
-        longtitudeRangeFilter.currentMin + 0.01,
-        latitudeRangeFilter.currentMax - 0.01,
-      ]).join(',')} 
-            ${projection([
-        longtitudeRangeFilter.currentMin + 0.01,
-        latitudeRangeFilter.currentMin + 0.01,
-      ]).join(',')}
-            ${projection([
-        longtitudeRangeFilter.currentMax - 0.01,
-        latitudeRangeFilter.currentMin + 0.01,
-      ]).join(',')}
-            ${projection([
-        longtitudeRangeFilter.currentMax - 0.01,
-        latitudeRangeFilter.currentMax - 0.01,
-      ]).join(',')}
-            ${projection([
-        longtitudeRangeFilter.currentMin + 0.01,
-        latitudeRangeFilter.currentMax - 0.01,
-      ]).join(',')} `}
+            ${projection([longtitudeCurrentMin, latitudeCurrentMax]).join(',')} 
+            ${projection([longtitudeCurrentMin, latitudeCurrentMin]).join(',')}
+            ${projection([longtitudeCurrentMax, latitudeCurrentMin]).join(',')}
+            ${projection([longtitudeCurrentMax, latitudeCurrentMax]).join(',')}
+            ${projection([longtitudeCurrentMin, latitudeCurrentMax]).join(',')} `}
             />
-            : ''}
+          ) : (
+            ''
+          )}
         </svg>
       </div>
     );
