2bf65371bdc3d0f778bee9978687320dbca89e4c
hdorothea <dorothea.hormann@gmail.com>
2017-09-18 19:00:58 -0400

    Misc styles

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="app">
       <Header />
-      <div className="content">
+      <div className="app-content">
         <FilterDash />
         <DashboardContainer />
       </div>
