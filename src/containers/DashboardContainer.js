d7b4c30ab8b0a2cd50e5fc7f6cef5cb9c04ae6a4
hdorothea <dorothea.hormann@gmail.com>
2018-01-19 18:59:35 -0800

    Make Dashboard gracefully degrade before data comes in

--- src/containers/DashboardContainer.js
+++ src/containers/DashboardContainer.js
@@ -2,48 +2,68 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import React from 'react';
 import DashBoard from '../components/DashBoard';
-import { keysToIndexApp, filterableValues } from '../utils/ApiUtils';
+import { keysToIndexApp } from '../utils/ApiUtils';
+
+const propTypes = {
+  observations: PropTypes.array,
+  latitudeRangeFilter: PropTypes.object,
+  longtitudeRangeFilter: PropTypes.object,
+  airlineCountryFilters: PropTypes.array,
+  showAll: PropTypes.bool
+};
+
+const defaultProps = {
+  observations: [],
+  latitudeRangeFilter: {},
+  longtitudeRangeFilter: {},
+  airlineCountryFilters: [],
+  showAll: true
+};
 
 class DashBoardContainer extends React.Component {
   getFilteredObservations() {
     if (this.props.showAll) {
       return this.props.observations;
     } else {
+      const latMax = this.props.latitudeRangeFilter.currentMax;
+      const latMin = this.props.latitudeRangeFilter.currentMin;
+      const longMax = this.props.longtitudeRangeFilter.currentMax;
+      const longMin = this.props.longtitudeRangeFilter.currentMin;
       return this.props.observations.filter(
         observation =>
           (this.props.airlineCountryFilters.length > 0
             ? this.props.airlineCountryFilters.includes(observation[keysToIndexApp.airlineCountry])
             : true) &&
-          this.props.latitudeRangeFilter.currentMax > observation[keysToIndexApp.latitude] &&
-          this.props.latitudeRangeFilter.currentMin < observation[keysToIndexApp.latitude] &&
-          this.props.longtitudeRangeFilter.currentMax > observation[keysToIndexApp.longtitude] &&
-          this.props.longtitudeRangeFilter.currentMin < observation[keysToIndexApp.longtitude],
+          (latMax ? latMax > observation[keysToIndexApp.latitude] : true) &&
+          (latMin ? latMin < observation[keysToIndexApp.latitude] : true) &&
+          (longMax ? longMax > observation[keysToIndexApp.longtitude] : true) &&
+          (longMin ? longMin < observation[keysToIndexApp.longtitude] : true)
       );
     }
   }
 
   render() {
-    if (this.props.observations.length > 0) {
-      return (
-        <DashBoard
-          latitudeRangeFilter={this.props.latitudeRangeFilter}
-          longtitudeRangeFilter={this.props.longtitudeRangeFilter}
-          observations={this.getFilteredObservations()}
-          showAll={this.props.showAll}
-        />
-      );
-    } else {
-      return null;
-    }
+    return (
+      <DashBoard
+        latitudeRangeFilter={this.props.latitudeRangeFilter}
+        longtitudeRangeFilter={this.props.longtitudeRangeFilter}
+        observations={this.getFilteredObservations() || []}
+        showAll={this.props.showAll}
+      />
+    );
   }
 }
+
+DashBoardContainer.propTypes = propTypes;
+
+DashBoardContainer.defaultProps = defaultProps;
 
 const mapStateToProps = state => ({
   observations: state.api.observations,
   showAll: state.filter.showAll,
   airlineCountryFilters: state.filter.airlineCountry,
   latitudeRangeFilter: state.filter.latitude,
-  longtitudeRangeFilter: state.filter.longtitude,
+  longtitudeRangeFilter: state.filter.longtitude
 });
 
 export default connect(mapStateToProps)(DashBoardContainer);
