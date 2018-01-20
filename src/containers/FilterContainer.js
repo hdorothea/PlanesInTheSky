f4cbbdbd3984e075a7a3f6f220f2625250cc8711
hdorothea <dorothea.hormann@gmail.com>
2018-01-19 18:59:53 -0800

    Add props

--- src/containers/FilterContainer.js
+++ src/containers/FilterContainer.js
@@ -1,16 +1,31 @@
 import { connect } from 'react-redux';
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { FilterView } from '../components/FilterView';
 import { addFilter } from '../actions/FilterActions';
 import { removeFilter } from '../actions/FilterActions';
 import { keysToIndexApp } from '../utils/ApiUtils';
 
+const propTypes = {
+  uniqueAirlineCountries: PropTypes.array,
+  addFilter: PropTypes.func,
+  removeFilter: PropTypes.func,
+  activeAirlineCountryFilterValues: PropTypes.array
+};
+
+const defaultProps = {
+  uniqueAirlineCountries: [],
+  addFilter: () => null,
+  removeFilter: () => null,
+  activeAirlineCountryFilterValues: () => null
+};
+
 function FilterContainer({
   uniqueAirlineCountries,
   addFilter,
   removeFilter,
-  activeAirlineCountryFilterValues,
+  activeAirlineCountryFilterValues
 }) {
   return (
     <FilterView
@@ -25,7 +40,7 @@ function FilterContainer({
 
 const mapDispatchToProps = dispatch => ({
   addFilter: (filterKey, filterValue) => dispatch(addFilter(filterKey, filterValue)),
-  removeFilter: (filterKey, filterValue) => dispatch(removeFilter(filterKey, filterValue)),
+  removeFilter: (filterKey, filterValue) => dispatch(removeFilter(filterKey, filterValue))
 });
 
 function getUniqueValues(observations, key) {
@@ -34,7 +49,10 @@ function getUniqueValues(observations, key) {
 
 const mapStateToProps = state => ({
   uniqueAirlineCountries: getUniqueValues(state.api.observations, 'airlineCountry'),
-  activeAirlineCountryFilterValues: state.filter.airlineCountry,
+  activeAirlineCountryFilterValues: state.filter.airlineCountry
 });
+FilterContainer.propTypes = propTypes;
+
+FilterContainer.defaultProps = defaultProps;
 
 export default connect(mapStateToProps, mapDispatchToProps)(FilterContainer);
